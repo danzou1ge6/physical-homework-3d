858148675fb2dafa9ed11c8ac1588ae63e5d8c8e
danzou1ge6 <yu_liukun@outlook.com>
2022-04-07 18:55:30 +0800

    change JieGe.png to JieGe.jpg

--- src/assets/displayIndex.js
+++ src/assets/displayIndex.js
@@ -2,25 +2,25 @@ const BASE_URL = import.meta.env.BASE_URL
 
 let index = {
     "sorakado-ai": {
-        center: "/SorakadoAi.jpg",
+        center: "SorakadoAi.jpg",
         outer: [
-            "/SorakadoAi.jpg",
-            "/SorakadoAi.jpg",
-            "/SorakadoAi.jpg",
-            "/SorakadoAi.jpg",
-            "/SorakadoAi.jpg",
-            "/SorakadoAi.jpg",
-            "/SorakadoAi.jpg"
+            "SorakadoAi.jpg",
+            "SorakadoAi.jpg",
+            "SorakadoAi.jpg",
+            "SorakadoAi.jpg",
+            "SorakadoAi.jpg",
+            "SorakadoAi.jpg",
+            "SorakadoAi.jpg"
         ]
     },
     "2022-04-07": {
-        center: "/JieGe.png",
+        center: "JieGe.jpg",
         outer: [
-            "/2022-04-07/1.png",
-            "/2022-04-07/2.png",
-            "/2022-04-07/3.png",
-            "/2022-04-07/4.png",
-            "/2022-04-07/5.png"
+            "2022-04-07/1.png",
+            "2022-04-07/2.png",
+            "2022-04-07/3.png",
+            "2022-04-07/4.png",
+            "2022-04-07/5.png"
         ]
     }
 }
