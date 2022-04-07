4210d890502966919b1216c034da16afce0e021d
danzou1ge6 <yu_liukun@outlook.com>
2022-04-07 16:13:33 +0800

    chore: reconfigure for deploy
    - set `base` to "physical-homework-3d"
    - un-ignore `./dist`

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: 'assets'
+  publicDir: 'assets',
+  base: '/physical-homework-3d/'
 })
